8158851b82c8c058decaa638fc08a03f72a0235f
Ilya Frolov <ilya+git@close.io>
2016-06-24 03:41:21 +0300

    Add setup.py for easier distribution

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='tz-trout',
+    version='0.1',
+    url='http://github.com/closeio/tz-trout',
+    license='MIT',
+    description='Helps you figure out the time zone based on an address or a phone number.',
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    packages=[
+        'tztrout',
+    ],
+)
