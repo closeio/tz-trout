ee386b772c8bdac65516ee54fa56099401771918
Vyacheslav Tverskoy <tsx@tsx.su>
2019-04-26 16:02:21 +0500

    Bump 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tz-trout',
-    version='0.2.0',
+    version='0.2.1',
     url='http://github.com/closeio/tz-trout',
     license='MIT',
     author='Close.io',
